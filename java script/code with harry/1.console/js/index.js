25b392cb4f1238a8f248f33b53d0473b19ff3572
kishan <kishantiwari8283877183q@gmail.com>
2022-09-21 17:29:36 +0530

    first commit

---
+++ java script/code with harry/1.console/js/index.js
@@ -0,0 +1,21 @@
+// console.log('CodeWithHarry');  
+// console.log(1); 
+// console.log(true); 
+// console.log(null); ;  
+// console.log([1, 2, 3]); // array inside log
+// console.table({name:"Harry", language:"JavaScript", tutorial:2});
+// console.assert(0>1, "Expression is false")
+// console.warn("This is a warning");
+//console.warn("This is a warning");
+// console.time();
+// for (i = 0; i < 100; i++) {
+//   // code
+//}
+// console.timeEnd();
+// console.group('simple'); 
+// console.log('Grouped'); 
+// console.groupEnd('simple'); 
+// console.log('new section');
+const spacing = '8px'; 
+const mystyle ='padding: ${spacing}; background-color: white; color: blue ; font-style:italic; border: 1px solid black dotted; font-size: 2em;'
+console.log('%cCode With Harry', mystyle);
